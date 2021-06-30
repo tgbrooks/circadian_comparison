9957df2394ab85e73e43704aef8e8f33aa4c78ae
Thomas Brooks <thobr@sas.upenn.edu>
2021-06-30 10:02:33 -0400

    Add note about setting up LSF support

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -4,6 +4,12 @@ module load sratoolkit-2.10.0
 module load salmon-v1.4.0
 source venv/bin/activate
 
+
+### SETUP NOTE:
+# In order to use the following --profile lsf command
+# you need to follow the instructions at https://github.com/Snakemake-Profiles/lsf
+# and set up LSF support for snakemake
+
 bsub -e logs/snakemake.err \
      -o logs/snakemake.out \
      snakemake --profile lsf -c 50
