b5f77ab59e3f6b2b1c7538ca071a1bdaf947fc71
Thomas Brooks <thobr@sas.upenn.edu>
2021-07-13 18:30:17 -0400

    Add edirect dependency

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,6 +3,7 @@ module load /project/itmatlab/sharedmodules/use.shared
 module load sratoolkit-2.11.0
 module load salmon-v1.4.0
 module load R/3.6.3
+module load edirect-15.3
 source venv/bin/activate
 
 
