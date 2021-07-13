e2fb9c692100eb14e96f4b46ab83b10774c3ae09
Thomas Brooks <thobr@sas.upenn.edu>
2021-07-13 16:02:00 -0400

    Use new version of sratoolkit

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 module load /project/itmatlab/sharedmodules/use.shared
-module load sratoolkit-2.10.0
+module load sratoolkit-2.11.0
 module load salmon-v1.4.0
 module load R/3.6.3
 source venv/bin/activate
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 module load /project/itmatlab/sharedmodules/use.shared
 module load salmon-v1.4.0
-module load sratoolkit-2.10.0
+module load sratoolkit-2.11.0
 module load R/3.6.3
 source venv/bin/activate
