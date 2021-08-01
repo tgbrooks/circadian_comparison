b653f255e8e2bb48d0af394969f52171b9283d77
Thomas Brooks <thobr@sas.upenn.edu>
2021-08-01 14:59:02 -0400

    Add heatmap of 'common' rhythmic genes

--- scripts/plot_overlapped_genes.py
+++ scripts/plot_overlapped_genes.py
@@ -5,7 +5,7 @@ import numpy
 import matplotlib
 matplotlib.use("Agg")
 import pylab
-
+from studies import sample_timepoints
 import util
 
 studies = snakemake.params.studies
@@ -34,15 +34,54 @@ results_df.to_csv(snakemake.output.num_common_genes, sep="\t")
 p_value_cutoff = 0.05
 selected_genes = {}
 for study, jtkfile in zip(studies, snakemake.input.jtk):
+    if study in ["Lahens15", "Manella21_Liver", "Zhang14_RNAseq_Liver_M"]:
+        continue
     jtk = pandas.read_csv(jtkfile, sep="\t", index_col=0)
     #jtk = jtk.sort_values("ADJ.P")
     selected_genes[study] = jtk.index[jtk["ADJ.P"]<p_value_cutoff]
 
-intersection = selected_genes[studies[0]]
-for study in studies:
+robustness_score = pandas.Series(0, index=jtk.index)
+
+intersection = selected_genes[studies[2]]
+for study in selected_genes.keys():
     genes = selected_genes[study]
     intersection = intersection.intersection(genes)
+    robustness_score[genes] += 1
 
 gene_name_from_id = pandas.read_csv("gene_name.txt", sep="\t", index_col="ID")['GeneSymbol']
 intersect_DF = pandas.DataFrame({"GeneID":intersection, "GeneSymbol":intersection.map(gene_name_from_id)})
 intersect_DF.to_csv(snakemake.output.common_genes_pvalue, sep="\t", index=False)
+robustness_score.to_csv(snakemake.output.robustness_score, sep="\t")
+
+data = {}
+metadata = []
+for study, tpmfile in zip(studies, snakemake.input.tpm):
+    if study == "Weger18":
+        continue
+    tpm = pandas.read_csv(tpmfile, sep="\t", index_col=0)
+    time = sample_timepoints(study)
+    metadata.append(pandas.DataFrame({
+        "study": [study for i in range(len(tpm.columns))],
+        "time": time
+    }, index=tpm.columns))
+    # Sort TPM value columns by time
+    #time_order = numpy.argsort(time)
+    #tpm = tpm.iloc[:, time_order]
+    data[study] = tpm
+
+data_df = pandas.concat(data.values(), axis=1)
+metadata_df = pandas.concat(metadata, axis=0)
+time_order = numpy.argsort(metadata_df.time%24)
+data_df = data_df.iloc[:, time_order]
+heatmap = numpy.log(data_df.loc[intersection]+1)
+heatmap = heatmap.divide(heatmap.median(axis=1), axis="index")
+fig, ax = pylab.subplots(figsize=(60,6))
+ax.imshow(heatmap, aspect = 5, interpolation='nearest')
+count_study = metadata_df.groupby("study", sort=False).size().cumsum()
+print(count_study)
+#ax.set_xticks(count_study)
+#ax.set_xticklabels(metadata_df.study.unique())
+ax.set_yticks(list(range(len(intersection))))
+ax.set_yticklabels(intersection)
+fig.tight_layout()
+fig.savefig(snakemake.output.heatmap, dpi=300)
