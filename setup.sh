76e7f1c584b815cd81183698142a79485acb6716
Thomas Brooks <thobr@sas.upenn.edu>
2021-08-07 17:09:20 -0400

    Add R library dependencies to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,13 @@ module load edirect-15.3
 source venv/bin/activate
 
 pip install -e .
+
+# The following R libraries must be installed prior to running:
+# 'devtools':
+# R> install.packages("devtools")
+# metacycle:
+# R> devtools::install_github('gangwug/MetaCycle')
+# ASSIST
+# R> install.packages("assist")
+# tidyr:
+# R> install.packages("tidyr")
