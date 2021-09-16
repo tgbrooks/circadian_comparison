55731f08efe3e26033f88e989c008a4e422bdb24
Thomas Brooks <thobr@sas.upenn.edu>
2021-09-16 11:48:21 -0400

    Make 'highlighted' studies use squares

--- styles.py
+++ styles.py
@@ -1,12 +1,13 @@
 import matplotlib
 import pylab
 
-from studies import studies
+from studies import studies, targets
 
 # Determine styles for each study
 cmap = pylab.get_cmap("tab20")
-marker = ["o", "+", "s", "v", "x"]
+marker = ["+", "s", "v", "x"]
 linestyles = ["-", "--", "-.", ".."]
 color_by_study = {study: cmap(i%20) for i, study in enumerate(studies)}
-shape_by_study = {study: marker[i//20] for i, study in enumerate(studies)}
+shape_by_study = {study: marker[i//20] if not targets[study].get('highlight',False) else 'o'
+                        for i, study in enumerate(studies)}
 linestyle_by_study = {study: linestyles[i//20] for i, study in enumerate(studies)}
