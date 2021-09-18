b863ea5ca029842c7881a28fe04363add0f9098b
Thomas Brooks <thobr@sas.upenn.edu>
2021-09-18 12:21:55 -0400

    Move name formatting to styles.py

--- styles.py
+++ styles.py
@@ -11,3 +11,22 @@ color_by_study = {study: cmap(i%20) for i, study in enumerate(studies)}
 shape_by_study = {study: marker[i//20] if not targets[study].get('highlight',False) else 'o'
                         for i, study in enumerate(studies)}
 linestyle_by_study = {study: linestyles[i//20] for i, study in enumerate(studies)}
+
+
+# Format study names into multi-lines
+def format_study_name(study, max_length=15):
+    parts = study.split("_")
+    line = parts[0]
+    lines = []
+    for part in parts[1:]:
+        if len(line) + len(part) + 1 > max_length:
+            lines.append(line)
+            line = part
+        else:
+            line = line + "_" + part
+    lines.append(line)
+    joined = '\n'.join(lines)
+
+    if targets[study].get("highlight", False):
+        joined = joined + "*"
+    return joined
