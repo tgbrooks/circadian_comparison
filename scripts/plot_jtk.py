db80039b08801b4573b2f3a9cfbe9b6f3d6fdbd9
tgbrooks <tgbrooks@gmail.com>
2022-04-08 16:28:33 -0400

    Add info to phase heatmap

--- scripts/plot_jtk.py
+++ scripts/plot_jtk.py
@@ -112,16 +112,30 @@ fig.tight_layout()
 fig.savefig(snakemake.output.phases, dpi=DPI)
 
 # Heatmap of phases
-fig, main_ax= pylab.subplots(figsize=(10,10), sharey=True)
-density_by_study = numpy.zeros(shape=(len(include_studies), len(bins)-1))
-for i, study in enumerate(include_studies):
+plot_studies = sorted(studies, key=lambda x: study_info[x]['short_name'])
+fig, axes = pylab.subplots(
+        figsize=(10,10),
+        sharey=True,
+        ncols=4,
+        gridspec_kw=dict(
+            width_ratios = (2, 2, 1, len(bins)-1),
+        ),
+        constrained_layout=True
+)
+(label_ax, age_ax, num_sig_ax, main_ax) = axes
+density_by_study = numpy.zeros(shape=(len(plot_studies), len(bins)-1))
+num_sig_by_study = numpy.zeros((len(plot_studies),1))
+for i, study in enumerate(plot_studies):
     phase = phases['loose'][study]
-    if len(phase) < 2:
+    num_sig_by_study[i,0] = len(phase)
+    if len(phase) < 10:
         continue
     #counts, edges = numpy.histogram(phase, bins)
-    #density_by_study[i,:]  = counts
-    kde = scipy.stats.gaussian_kde(phase.values)
-    vals = kde(bins[:-1]+0.5) * len(phase)
+    #density_by_study[i,:]  = counts / sum(counts)
+    #num_sig_by_study[i,0] = sum(counts)
+    kde = scipy.stats.gaussian_kde(phase.values, bw_method=0.3)
+    vals = kde(bins[:-1]+0.5)# * len(phase)
+    vals /= max(vals) # Maximum is 1
     density_by_study[i,:] = vals
 cmap = matplotlib.cm.get_cmap().copy()
 #cmap.set_bad(cmap(0))
@@ -129,23 +143,53 @@ cmap = matplotlib.cm.get_cmap().copy()
 h = main_ax.imshow(
     density_by_study,
     cmap = cmap,
-    norm = matplotlib.colors.LogNorm(vmin=1),
+    #norm = matplotlib.colors.LogNorm(vmin=1),
 )
-# study info values
-main_ax.imshow(
-    [[color_by_sex[study_info[study]['sex']], color_by_light[study_info[study]['light']]]
-        for study in include_studies],
-    extent = (-3.5,-1.5,-0.5, len(include_studies)-0.5)
-)
-main_ax.set_xlim(-3.5, len(bins)-1-0.5)
-main_ax.set_yticks(numpy.arange(len(include_studies)))
-main_ax.set_yticklabels([study_info[study]['short_name'] for study in include_studies])
+main_ax.set_xlim(-0.5, len(bins)-1-0.5)
 time_labels = numpy.arange(0, jtk_period+1, jtk_period//4)
-main_ax.set_xticks(numpy.concatenate(([-3,-2],time_labels))-0.5)
-main_ax.set_xticklabels(['sex', 'light'] + [str(x) for x in time_labels])
+main_ax.set_xticks(time_labels-0.5)
+main_ax.set_xticklabels([str(x) for x in time_labels])
 main_ax.set_xlabel("Phase (hrs)")
-fig.colorbar(h)
+# study info values
+label_ax.imshow(
+    [[
+        color_by_sex[study_info[study]['sex']],
+        color_by_light[study_info[study]['light']],
+        ]
+        for study in plot_studies],
+)
+label_ax.set_xticks([0,1])
+label_ax.set_xticklabels(['sex', 'light'], rotation=90)
+label_ax.set_yticks(numpy.arange(len(plot_studies)))
+label_ax.set_yticklabels([study_info[study]['short_name'] for study in plot_studies])
+# Age axis
+h3 = age_ax.imshow(
+    [[
+        study_info[study]['age_low'],
+        study_info[study]['age_high'],
+    ] for study in plot_studies],
+    cmap = "plasma",
+)
+age_ax.set_xticks([0,1])
+age_ax.set_xticklabels(['age low', 'age high'], rotation=90)
+# Num sig axis
+h2 = num_sig_ax.imshow(
+        num_sig_by_study,
+        cmap = "inferno",
+        norm = matplotlib.colors.SymLogNorm(linthresh=1),
+)
+num_sig_ax.set_xticks([0])
+num_sig_ax.set_xticklabels(['num. rhythmic'], rotation=90)
+#Colorbars + legend
+fig.colorbar(h2, ax=axes, label="Num. Rhythmic", fraction=0.025)
+fig.colorbar(h, ax=axes, label="Phase Density", fraction=0.025)
+fig.colorbar(h3, ax=axes, label="Age (weeks)", fraction=0.025)
+util.legend_from_colormap(
+    fig = fig,
+    colormap = {**color_by_sex, **color_by_light},
+)
 fig.savefig(snakemake.output.phase_heatmap, dpi=DPI)
+fig.savefig(snakemake.output.phase_heatmap_svg)
 
 # Histogram of amplitudes
 fig, axes = pylab.subplots(figsize=(1+5*N_COLUMNS,0.7+0.7*N_ROWS), nrows=N_ROWS, ncols=N_COLUMNS, sharex=True, squeeze=False)
