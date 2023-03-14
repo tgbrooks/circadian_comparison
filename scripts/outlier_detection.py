ed89aa1aadb7199064d6634b1fa3f93c3bc49e57
tgbrooks <tgbrooks@gmail.com>
2023-03-14 10:17:32 -0400

    Switch outlier detection code to single-study

--- scripts/outlier_detection.py
+++ scripts/outlier_detection.py
@@ -13,10 +13,7 @@ import scipy.sparse.linalg
 ZSCORE_OUTLIER_THRESHOLD = 3
 
 data = pandas.read_csv(snakemake.input.tpm, sep="\t", index_col=[0,1])
-sample_info = pandas.read_csv(snakemake.input.sample_info, sep="\t", index_col=[0])
 print(data.shape)
-print(sample_info.shape)
-assert (data.columns == sample_info.index).all()
 
 data = numpy.log(data + 0.01) # Log transform the data
 
@@ -28,23 +25,22 @@ def standardize(x):
     return std
 
 outliers = set()
-for study, study_data in data.groupby(sample_info.study, axis=1):
 
-    is_outlier = pandas.Series(False, index=study_data.columns)
-    while True:
-        drop = is_outlier[study_data.columns]
-        std_data = standardize(study_data.loc[:,~drop])
-        u, s, dt = scipy.sparse.linalg.svds(std_data, k=1)
+is_outlier = pandas.Series(False, index=data.columns)
+while True:
+    drop = is_outlier[data.columns]
+    std_data = standardize(data.loc[:,~drop])
+    u, s, dt = scipy.sparse.linalg.svds(std_data, k=1)
 
-        scores = (std_data.T @ u)[0]
-        scores_std = numpy.std(scores)
-        is_new_outlier = scores.abs()  - scores.mean() > ZSCORE_OUTLIER_THRESHOLD * scores_std
-        if sum(is_new_outlier) == 0:
-            break
-        is_outlier[is_new_outlier.index[is_new_outlier]] = True
-    outliers = outliers.union(is_outlier.index[is_outlier])
-    print(f"Identified {sum(is_outlier)} in {study}")
-    print('\n'.join(is_outlier.index[is_outlier]))
+    scores = (std_data.T @ u)[0]
+    scores_std = numpy.std(scores)
+    is_new_outlier = scores.abs()  - scores.mean() > ZSCORE_OUTLIER_THRESHOLD * scores_std
+    if sum(is_new_outlier) == 0:
+        break
+    is_outlier[is_new_outlier.index[is_new_outlier]] = True
+outliers = outliers.union(is_outlier.index[is_outlier])
+print(f"Identified {sum(is_outlier)} in {snakemake.wildcards.study}")
+print('\n'.join(is_outlier.index[is_outlier]))
 
 print(f"Identified {len(outliers)} in total")
 pandas.Series(list(outliers)).to_csv(snakemake.output.outliers, sep="\t", index=False, header=False)
